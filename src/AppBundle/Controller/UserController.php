4be380955c619f62927dcc38c741e24c185b2c05
Oriol Gasset <romo149@gmail.com>
2016-06-20 19:43:58 +0200

    User management: forms

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+    /**
+     * @Route(path ="/", name="app_user_index")
+     */
+    public function indexAction()
+    {
+        $m = $this->getDoctrine()->getManager();
+        $repository = $m->getRepository('AppBundle:User');
+        $users = $repository->findAll();
+
+        return $this->render(':user:index.html.twig', array('users' => $users));
+    }
+
+
+    /**
+     * @Route(path ="/insert", name="app_user_insert")
+     */
+    public function insertAction()
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        return $this->render(':user:form.html.twig', [
+            'form' => $form->createView(),
+            'action' => $this->generateUrl('app_user_doInsert')
+        ]);
+    }
+
+    /**
+     * @Route(path ="/doInsert", name="app_user_doInsert")
+     */
+    public function doInsert(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $m = $this->getDoctrine()->getManager();
+            $m->persist($user);
+            $m->flush();
+
+            $this->addFlash('messages', 'User added');
+
+            return $this->redirectToRoute('app_user_index');
+        }
+
+        $this->addFlash('messages', 'Review your form data');
+
+        return $this->render(':user:form.html.twig',
+            [
+                'form' => $form->createView(),
+                'action' => $this->generateUrl('app_user_doInsert')
+            ]
+        );
+
+    }
+
+    /**
+     * @Route(path ="/update/{id}", name="app_user_update")
+     */
+    public function updateFunction($id)
+    {
+        $m = $this->getDoctrine()->getManager();
+        $repository = $m->getRepository('AppBundle:User');
+        $user = $repository->find($id);
+        $form = $this->createForm(UserType::class, $user);
+
+        return $this->render(':user:form.html.twig', [
+            'form' => $form->createView(),
+            'action' => $this->generateUrl('app_user_doUpdate', ['id' => $id])
+        ]);
+
+    }
+
+    /**
+     * @Route(path ="/doUpdate/{id}", name="app_user_doUpdate")
+     */
+    public function doUpdate(Request $request, $id)
+    {
+        $m = $this->getDoctrine()->getManager();
+        $repository = $m->getRepository('AppBundle:User');
+        $user = $repository->find($id);
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $m = $this->getDoctrine()->getManager();
+            $m->flush();
+
+            $this->addFlash('messages', 'User updated');
+
+            return $this->redirectToRoute('app_user_index');
+        }
+
+        $this->addFlash('messages', 'Review your form data');
+
+        return $this->render(':user:form.html.twig',
+            [
+                'form' => $form->createView(),
+                'action' => $this->generateUrl('app_user_doUpdate', ['id' => $id])
+            ]
+        );
+    }
+
+    /**
+     * @Route(path="/delete/{id}", name="app_user_delete")
+     */
+    public function doDelete($id)
+    {
+        $m = $this->getDoctrine()->getManager();
+        $repository = $m->getRepository('AppBundle:User');
+        $user = $repository->find($id);
+        $m->remove($user);
+        $m->flush();
+
+        $this->addFlash('messages', 'User deleted');
+
+        return $this->redirectToRoute('app_user_index');
+    }
+
+}
